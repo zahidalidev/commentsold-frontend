34a0dcd533cdf1421205228f57a30278105d86ed
zahidalidev <m.zahidalidev@gmail.com>
2022-08-20 03:01:20 +0500

    product test cases

--- src/__tests__/containers/orders/index.test.js
+++ src/__tests__/containers/orders/index.test.js
@@ -1,4 +1,6 @@
-import { render, screen, waitFor, act } from '@testing-library/react'
+import {
+  render, screen, waitFor, act,
+} from '@testing-library/react'
 
 import Orders from 'container/orders'
 import { TestApp } from 'utils/test'
@@ -22,23 +24,4 @@ describe('Orders Container', () => {
       expect(screen.getByTestId('table')).toBeInTheDocument()
     })
   })
-
-  // it('should have 2 headings', async () => {
-  //   await waitFor(() => {
-  //     expect(screen.getAllByRole('heading').length).toBe(2)
-  //   })
-  // })
-
-  // it('should have select component', async () => {
-  //   await waitFor(() => {
-  //     expect(screen.getAllByTestId('select')).toBeInTheDocument()
-  //   })
-  // })
-
-  // it('should have textbox', async () => {
-  //   await waitFor(() => {
-  //     const textbox = screen.getAllByRole('textbox')
-  //     expect(textbox).toHaveLength(1)
-  //   })
-  // })
 })
